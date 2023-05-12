9612b4898b400eace7c1447f06674ee16b90a55d
Esdras Abreu <xstrikeesdraspb@gmail.com>
2023-05-12 14:07:05 -0300

    atualizacao 5

--- app/page.js
+++ app/page.js
@@ -37,23 +37,6 @@ function ProductRow({ product }) {
   );
 }
 
-function SearchBar({ filterText, inStockOnly, onFilterTextChange,
-  onInStockOnlyChange }) {
-  return (
-    <form>
-      <input type="text" value={filterText} placeholder="Search..."
-        onChange={(e) => onFilterTextChange(e.target.value)} />
-      <label>
-        <input type="checkbox"
-          value={inStockOnly}
-          onChange={(e) => onInStockOnlyChange(e.target.checked)}
-        />
-        {' '}
-        Only show products in stock
-      </label>
-    </form>
-  );
-}
 
 function ProductTable({ products, filterText, inStockOnly }) {
   const rows = [];
