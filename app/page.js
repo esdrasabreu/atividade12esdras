0e04f8823a098b32400b00b51d9df959a815943a
Esdras Abreu <xstrikeesdraspb@gmail.com>
2023-05-12 13:54:18 -0300

    atualizaçao 2

--- app/page.js
+++ app/page.js
@@ -1,17 +1,15 @@
-"use client"; 
-
 import Image from 'next/image'
 import styles from './page.module.css'
 import React from 'react';
 import { useState } from 'react';
 
 const PRODUCTS = [
-  {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
-  {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
-  {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
-  {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
-  {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
-  {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
+  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
 ];
 
 function ProductCategoryRow({ category }) {
@@ -38,16 +36,16 @@ function ProductRow({ product }) {
   );
 }
 
-function SearchBar({filterText, inStockOnly, onFilterTextChange,
-  onInStockOnlyChange}) {
+function SearchBar({ filterText, inStockOnly, onFilterTextChange,
+  onInStockOnlyChange }) {
   return (
     <form>
-      <input type="text" value={filterText} placeholder="Search..." 
-      onChange={(e) => onFilterTextChange(e.target.value)}/>
+      <input type="text" value={filterText} placeholder="Search..."
+        onChange={(e) => onFilterTextChange(e.target.value)} />
       <label>
-        <input type="checkbox" 
-        value={inStockOnly}
-        onChange={(e) => onInStockOnlyChange(e.target.checked)}
+        <input type="checkbox"
+          value={inStockOnly}
+          onChange={(e) => onInStockOnlyChange(e.target.checked)}
         />
         {' '}
         Only show products in stock
@@ -56,7 +54,7 @@ function SearchBar({filterText, inStockOnly, onFilterTextChange,
   );
 }
 
-function ProductTable({ products, filterText, inStockOnly}) {
+function ProductTable({ products, filterText, inStockOnly }) {
   const rows = [];
   let lastCategory = null;
   products.forEach((product) => {
@@ -100,40 +98,115 @@ function FilterableProductTable({ products }) {
 
   return (
     <div>
-      <SearchBar filterText={filterText}  inStockOnly={inStockOnly} 
-      onFilterTextChange={setFilterText}
-      onInStockOnlyChange={setInStockOnly}/>
-      <ProductTable products={products} filterText={filterText}  inStockOnly={inStockOnly}/>
+      <SearchBar filterText={filterText} inStockOnly={inStockOnly}
+        onFilterTextChange={setFilterText}
+        onInStockOnlyChange={setInStockOnly} />
+      <ProductTable products={products} filterText={filterText} inStockOnly={inStockOnly} />
     </div>
-  ); 
+  );
 }
-let App = function App() {
-  return (<>
-            <FilterableProductTable products={PRODUCTS} />
-          </>)
-}
-const root = createRoot(document.getElementById('root'));
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
 
-
-export default function Home() {
-    
+function Home() {
   const [blogMessages, setBlogMessages] = useState([]);
-  
-  fetch('https://...')
-    .then(response => response.json())
-    .then(data => {
+
+  useEffect(() => {
+    fetch('https://...')
+      .then(response => response.json())
+      .then(data => {
         setBlogMessages(data);
-    });
-    
-    return (
-      <main className={styles.main}>
-        <FilterableMessageTable messages={blogMessages} />
-      </main>
-    )
+      })
+      .catch(error => {
+        console.error('Error fetching blog messages:', error);
+      });
+  }, []);
+
+  return (
+    <main className={styles.main}>
+      <FilterableMessageTable messages={blogMessages} />
+    </main>
+  );
 }
 
+function MessageRow({ message }) {
+  return (
+    <tr>
+      <td>{message.author}</td>
+      <td>{message.subject}</td>
+      <td>{message.body}</td>
+    </tr>
+  );
+}
+
+function MessageCategoryRow({ category }) {
+  return (
+    <tr>
+      <th colSpan="3">{category}</th>
+    </tr>
+  );
+}
+
+function SearchBar({ filterText, onFilterTextChange }) {
+  return (
+    <form>
+      <input
+        type="text"
+        placeholder="Search..."
+        value={filterText}
+        onChange={(e) => onFilterTextChange(e.target.value)}
+      />
+    </form>
+  );
+}
+
+function MessageTable({ messages, filterText }) {
+  const rows = [];
+  let lastCategory = null;
+
+  messages.forEach((message) => {
+    if (message.subject.toLowerCase().indexOf(filterText.toLowerCase()) === -1) {
+      return;
+    }
+    if (message.category !== lastCategory) {
+      rows.push(
+        <MessageCategoryRow
+          category={message.category}
+          key={message.category}
+        />
+      );
+    }
+    rows.push(<MessageRow message={message} key={message.id} />);
+    lastCategory = message.category;
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Author</th>
+          <th>Subject</th>
+          <th>Body</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
+}
+
+function FilterableMessageTable({ messages }) {
+  const [filterText, setFilterText] = useState('');
+
+  const filteredMessages = messages.filter((message) => {
+    return message.author.toLowerCase().indexOf(filterText.toLowerCase()) !== -1 ||
+      message.subject.toLowerCase().indexOf(filterText.toLowerCase()) !== -1 ||
+      message.body.toLowerCase().indexOf(filterText.toLowerCase()) !== -1;
+  });
+
+  return (
+    <div>
+      <SearchBar filterText={filterText} onFilterTextChange={setFilterText} />
+      <MessageTable messages={filteredMessages} filterText={filterText} />
+    </div>
+  );
+}
+
+export default Home;
